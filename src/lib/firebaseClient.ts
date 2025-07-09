b8776c9f3a72c2fafc25f1f4b790239719581c33
AhmadToha220505 <ahmadtohaahmad22@gmail.com>
2025-07-09 23:09:38 +0700

    Initial commit

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -1,23 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDMSnAz0cUyna1R9YRSNXqyICWCcJZK1_4",
-  authDomain: "absensi-c55e7.firebaseapp.com",
-  projectId: "absensi-c55e7",
-  storageBucket: "absensi-c55e7.firebasestorage.app",
-  messagingSenderId: "515568133881",
-  appId: "1:515568133881:web:8f20d839dc0ced3831a428",
-  measurementId: "G-DEW9ZC9SHW",
+  apiKey: "AIzaSyDYISnNhVbKUJnBqyd1tXLmeGyM5HOKQZE",
+  authDomain: "absensi-737a6.firebaseapp.com",
+  projectId: "absensi-737a6",
+  storageBucket: "absensi-737a6.firebasestorage.app",
+  messagingSenderId: "418456264948",
+  appId: "1:418456264948:web:068cc4033e1cdde4df3ea1",
+  measurementId: "G-JNQQLWYY76"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
