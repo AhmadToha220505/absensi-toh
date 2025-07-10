5226e7a4b7de60843c7ff2dbf379787cb20a408c
AhmadToha220505 <ahmadtohaahmad22@gmail.com>
2025-07-10 11:33:41 +0700

    Initial commit

--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -1,11 +1,12 @@
+// src/lib/firebaseClient.ts
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDYISnNhVbKUJnBqyd1tXLmeGyM5HOKQZE",
   authDomain: "absensi-737a6.firebaseapp.com",
@@ -19,3 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// ✅ Tambahkan ini
+export const auth = getAuth(app);
+export const db = getFirestore(app);
